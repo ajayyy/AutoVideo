d33ccf1e1b347ed2d9d461b44c7965025ce98711
Ajay Ramachandran <dev@ajay.app>
2019-05-23 20:50:33 -0400

    made it save an image of the text

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const requests = require("request");
+var text2png = require('text2png');
+
 const TextToSpeechV1 = require("ibm-watson/text-to-speech/v1");
 
 //remove swearing
@@ -30,8 +32,16 @@ requests.get('https://www.reddit.com/r/askreddit/top.json?t=all', {}, function(e
 
     let randomPostIndex = Math.floor(Math.random() * body.data.children.length);
 
+    let cleanText = getCleanText(posts[randomPostIndex].data.title);
+    let displayText = getDisplayText(cleanText, posts[randomPostIndex].data);
+
+    //create image
+    fs.writeFile('processed/title.png', text2png(displayText, {color: 'white'}), function (err) {
+        if (err) console.log(err);
+    });
+
     let synthesizeParams = {
-        text: posts[randomPostIndex].data.title,
+        text: cleanText,
         accept: 'audio/wav',
         voice: 'en-US_MichaelVoice',
     };
@@ -49,7 +59,7 @@ requests.get('https://www.reddit.com/r/askreddit/top.json?t=all', {}, function(e
 
 function getComments(post) {
     let url = post.data.url;
-    // url = "https://google.com/aa"
+
     requests.get(url.substring(0, url.length - 2) + ".json", {}, function(err, res, body) {
         let response = JSON.parse(body);
 
@@ -63,19 +73,16 @@ function getComments(post) {
             if (urls != null) {
                 for (let i = 0; i < urls.length; i++) {
                     text = text.replace(urls[i], "");
-                    console.log(urls[i])
                 }
             }
     
-            let cleanText = customFilter.clean(text);
-    
-            var swears = cleanText.match(/\b (\+\-\~12)+/gi);
-    
-            if (swears != null) {
-                for (let i = 0; i < swears.length; i++) {
-                    cleanText = cleanText.replace(swears[i], " bleep");
-                }
-            }
+            let cleanText = getCleanText(text);
+            let displayText = getDisplayText(cleanText, comments[i].data);
+
+            //create image
+            fs.writeFile('processed/comment_' + i + '.png', text2png(displayText, {color: 'white'}), function (err) {
+                if (err) console.log(err);
+            });
     
             let synthesizeParams = {
                 text: cleanText,
@@ -92,4 +99,43 @@ function getComments(post) {
             });
         }
     });
+}
+
+function getCleanText(text) {
+    let cleanText = customFilter.clean(text);
+
+    var swears = cleanText.match(/\b (\+\-\~12)+/gi);
+
+    if (swears != null) {
+        for (let i = 0; i < swears.length; i++) {
+            cleanText = cleanText.replace(swears[i], " bleep");
+        }
+    }
+
+    return cleanText;
+}
+
+function getDisplayText(cleanText, comment) {
+    let displayText = cleanText.replace(/\b\+\-\~12/gi, "*");
+    displayText = displayText.replace(/\b \+\-\~12/gi, " *");
+    displayText = stringDivider(displayText, 70, "\n");
+
+    displayText += "\n\n - " + comment.author;
+
+    return displayText;
+}
+
+//from https://stackoverflow.com/a/14502311/1985387
+function stringDivider(str, width, spaceReplacer) {
+    if (str.length>width) {
+        var p=width
+        for (;p>0 && str[p]!=' ';p--) {
+        }
+        if (p>0) {
+            var left = str.substring(0, p);
+            var right = str.substring(p+1);
+            return left + spaceReplacer + stringDivider(right, width, spaceReplacer);
+        }
+    }
+    return str;
 }
